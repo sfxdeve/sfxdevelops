3b422a27d83e7eed30880d0c9573fe1c5461ec3f
Shayan Fareed <sfx.deve@gmail.com>
2026-02-13 18:33:06 +0500

    Add fmt and lint scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-
-
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
