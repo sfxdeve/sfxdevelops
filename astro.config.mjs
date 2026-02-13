c7438d615f4b466a0d05d485d3c723281b16a8d7
Shayan Fareed <sfx.deve@gmail.com>
2026-02-13 18:53:53 +0500

    Summarize config & deps updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: netlify(),
 });
